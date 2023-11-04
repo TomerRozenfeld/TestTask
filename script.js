8a6daa0a91a1e94254b1d83c4e122c6121949163
TomerRozenfeld <tomer18954@gmail.com>
2023-11-05 01:46:32 +0200

    task 2 didnt mangae 2-3

--- script.js
+++ script.js
@@ -1,46 +1,104 @@
-// script.js
-const apiKey = 'Y35829162-4638e1e58dac695ba18841589'; // Replace with your actual Pixabay API key
+const apiKey = '35829162-4638e1e58dac695ba18841589';
 
-document.getElementById('searchButton').addEventListener('click', searchPixabay);
+// Event listeners
+document.getElementById('search').addEventListener('input', clearResults);
+document.getElementById('myButton').addEventListener('click', showmorepics);
 
 function searchPixabay() {
-    const searchQuery = document.getElementById('search').value;
-    const url = `https://pixabay.com/api/?key=35829162-4638e1e58dac695ba18841589&q=yellow+flowers&image_type=photo`;
+    const searchQuery = document.getElementById('search').value.trim();
 
+    if (searchQuery) {
+        const queries = searchQuery.split(' ');
+
+        clearResults();
+
+        queries.forEach(query => {
+            const url = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo`;
+            fetchImages(url, 'results');
+        });
+    }
+}
+
+function clearResults() {
+    const results = document.getElementById('results');
+    results.innerHTML = '';
+
+    hideElement('myButton');
+    hideElement('myhead2');
+}
+
+function fetchImages(url, targetElementId) {
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            const results = document.getElementById('results');
+            const results = document.getElementById(targetElementId);
             results.innerHTML = '';
 
             data.hits.forEach(image => {
-                const card = document.createElement('div');
-                card.className = 'card';
-
-                const img = document.createElement('img');
-                img.src = image.webformatURL;
-
-                const cardContent = document.createElement('div');
-                cardContent.className = 'card-content';
-
-                const cardTitle = document.createElement('div');
-                cardTitle.className = 'card-title';
-                cardTitle.innerText = image.tags;
-
-                const cardDescription = document.createElement('div');
-                cardDescription.className = 'card-description';
-                cardDescription.innerText = `By ${image.user}`;
-
-                cardContent.appendChild(cardTitle);
-                cardContent.appendChild(cardDescription);
-
-                card.appendChild(img);
-                card.appendChild(cardContent);
-
-                results.appendChild(card);
+                const cardContainer = createCard(image);
+                results.appendChild(cardContainer);
             });
+
+            showElement('myButton');
+            showElement('myhead2');
         })
         .catch(error => {
             console.error('Error:', error);
         });
+}
+
+function createCard(image) {
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('card');
+
+    const img = document.createElement('img');
+    img.src = image.webformatURL;
+
+    const title = document.createElement('h3');
+    title.textContent = image.tags;
+
+    const author = document.createElement('p');
+    author.textContent = `By ${image.user}`;
+
+    cardContainer.appendChild(img);
+    cardContainer.appendChild(title);
+    cardContainer.appendChild(author);
+
+    return cardContainer;
+}
+
+function openModal(title, description) {
+    const modalTitle = document.getElementById('modalTitle');
+    const modalDescription = document.getElementById('modalDescription');
+    const modal = document.getElementById('myModal');
+
+    modalTitle.innerText = title;
+    modalDescription.innerText = description;
+
+    showElement('modal');
+}
+
+function closeModal() {
+    hideElement('modal');
+}
+
+function showElement(elementId) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.style.display = 'block';
+    }
+}
+
+function hideElement(elementId) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.style.display = 'none';
+    }
+}
+
+function showmorepics() {
+    const searchQuery = document.getElementById('search').value.trim();
+    const url = `https://pixabay.com/api/?key=${apiKey}&q=dogs&image_type=photo`;
+
+    fetchImages(url, 'Morepics');
 }

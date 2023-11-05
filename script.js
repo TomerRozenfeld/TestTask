05ec733f44bed833b73ba34a9c76cd04ddb6e6da
TomerRozenfeld <tomer18954@gmail.com>
2023-11-05 03:19:28 +0200

    step 3

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 const apiKey = '35829162-4638e1e58dac695ba18841589';
-
-// Event listeners
-document.getElementById('search').addEventListener('input', clearResults);
-document.getElementById('myButton').addEventListener('click', showmorepics);
+const predefinedTags = ["Background", "Wallpaper", "Natural", "Animals", "City", "Travel"];
 
 function searchPixabay() {
     const searchQuery = document.getElementById('search').value.trim();
-
     if (searchQuery) {
         const queries = searchQuery.split(' ');
-
         clearResults();
-
         queries.forEach(query => {
             const url = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo`;
             fetchImages(url, 'results');
@@ -20,11 +14,35 @@ function searchPixabay() {
 }
 
 function clearResults() {
-    const results = document.getElementById('results');
-    results.innerHTML = '';
+    document.getElementById('results').innerHTML = '';
+    document.getElementById('myButton').style.display = 'none';
+    document.getElementById('myhead2').style.display = 'none';
+}
 
-    hideElement('myButton');
-    hideElement('myhead2');
+function createImageCard(image) {
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('card');
+    const img = document.createElement('img');
+    img.src = image.webformatURL;
+    img.alt = image.tags;
+    const title = document.createElement('h3');
+    title.textContent = image.tags;
+    const author = document.createElement('p');
+    author.textContent = `By ${image.user}`;
+    const favoriteIcon = document.createElement('i');
+    favoriteIcon.classList.add('far', 'fa-heart', 'favorite');
+    favoriteIcon.addEventListener('click', () => toggleFavorite(favoriteIcon));
+    cardContainer.appendChild(favoriteIcon);
+    cardContainer.appendChild(img);
+    cardContainer.appendChild(title);
+    cardContainer.appendChild(author);
+    cardContainer.addEventListener('mouseenter', () => {
+        cardContainer.style.transform = 'scale(1.1)';
+    });
+    cardContainer.addEventListener('mouseleave', () => {
+        cardContainer.style.transform = 'scale(1.0)';
+    });
+    return cardContainer;
 }
 
 function fetchImages(url, targetElementId) {
@@ -33,72 +51,59 @@ function fetchImages(url, targetElementId) {
         .then(data => {
             const results = document.getElementById(targetElementId);
             results.innerHTML = '';
-
             data.hits.forEach(image => {
-                const cardContainer = createCard(image);
-                results.appendChild(cardContainer);
+                results.appendChild(createImageCard(image));
             });
-
-            showElement('myButton');
-            showElement('myhead2');
+            document.getElementById('myButton').style.display = 'block';
+            document.getElementById('myhead2').style.display = 'block';
         })
         .catch(error => {
             console.error('Error:', error);
         });
 }
 
-function createCard(image) {
-    const cardContainer = document.createElement('div');
-    cardContainer.classList.add('card');
-
-    const img = document.createElement('img');
-    img.src = image.webformatURL;
-
-    const title = document.createElement('h3');
-    title.textContent = image.tags;
-
-    const author = document.createElement('p');
-    author.textContent = `By ${image.user}`;
-
-    cardContainer.appendChild(img);
-    cardContainer.appendChild(title);
-    cardContainer.appendChild(author);
-
-    return cardContainer;
-}
-
-function openModal(title, description) {
-    const modalTitle = document.getElementById('modalTitle');
-    const modalDescription = document.getElementById('modalDescription');
-    const modal = document.getElementById('myModal');
-
-    modalTitle.innerText = title;
-    modalDescription.innerText = description;
-
-    showElement('modal');
-}
-
-function closeModal() {
-    hideElement('modal');
-}
-
-function showElement(elementId) {
-    const element = document.getElementById(elementId);
-    if (element) {
-        element.style.display = 'block';
+function toggleFavorite(favoriteIcon) {
+    if (favoriteIcon.classList.contains('favorited')) {
+        favoriteIcon.classList.remove('favorited');
+        showPopup('Image removed from favorites!');
+    } else {
+        favoriteIcon.classList.add('favorited');
+        showPopup('Image added to favorites!');
     }
 }
 
-function hideElement(elementId) {
-    const element = document.getElementById(elementId);
-    if (element) {
-        element.style.display = 'none';
-    }
+function showPopup(message) {
+    const popup = document.createElement('div');
+    popup.textContent = message;
+    popup.style.position = 'fixed';
+    popup.style.top = '10px';
+    popup.style.right = '10px';
+    popup.style.background = 'rgba(0, 0, 0, 0.7)';
+    popup.style.color = '#fff';
+    popup.style.padding = '10px';
+    popup.style.borderRadius = '5px';
+    popup.style.zIndex = '999';
+    document.body.appendChild(popup);
+    setTimeout(() => {
+        document.body.removeChild(popup);
+    }, 3000);
 }
 
 function showmorepics() {
     const searchQuery = document.getElementById('search').value.trim();
-    const url = `https://pixabay.com/api/?key=${apiKey}&q=dogs&image_type=photo`;
-
+    const url = `https://pixabay.com/api/?key=35829162-4638e1e58dac695ba18841589&q=dogs&image_type=photo`;
     fetchImages(url, 'Morepics');
 }
+
+function populatePredefinedTags() {
+    const select = document.getElementById('tags');
+    predefinedTags.forEach(tag => {
+        const option = document.createElement('option');
+        option.value = tag;
+        option.text = tag;
+        select.insertBefore(option, select.firstChild);
+    });
+}
+
+window.addEventListener('load', populatePredefinedTags);
+document.getElementById('tags').addEventListener('change', searchPixabay);
